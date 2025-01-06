13decc4f5ff99bb7c4d89b74700f8d22be0c0991
Laura Głąb <151255733+LauraGlab@users.noreply.github.com>
2025-01-06 02:04:59 +0100

    Update NavBar.jsx

--- src/menu/NavBar.jsx
+++ src/menu/NavBar.jsx
@@ -51,13 +51,25 @@ export default function NavBar() {
           </button>
         </div>
         <nav className="navBarSection" id="navBar">
-          <img
+          <div
             className="hamburgerMenu"
             src={HamburgerIcon}
             onClick={() => handleToggle()}
             alt="hamburger menu icon"
             aria-label="Open the menu"
-          />
+          >
+            <svg width="50px" height="50px" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg">
+              <g id="color" />
+              <g id="hair" />
+              <g id="skin" />
+              <g id="skin-shadow" />
+              <g id="line">
+                <line x1="16" x2="56" y1="26" y2="26" fill="none" stroke="#897455" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
+                <line x1="16" x2="56" y1="36" y2="36" fill="none" stroke="#897455" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
+                <line x1="16" x2="56" y1="46" y2="46" fill="none" stroke="#897455" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
+              </g>
+            </svg>
+          </div>
           {showMenu && (
             <Menu
               onClose={() => {
