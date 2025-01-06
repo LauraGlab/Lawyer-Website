7d40abd5d4d5527bc742c978aa2461a284bdadef
Laura Głąb <151255733+LauraGlab@users.noreply.github.com>
2025-01-06 22:16:28 +0100

    Update ErrorPage.jsx

--- src/sections/ErrorPage.jsx
+++ src/sections/ErrorPage.jsx
@@ -5,6 +5,7 @@ export default function ErrorPage() {
   return (
     <div className="error">
       <img className="errorLogo" src={Logo} alt="logo"/>
+      <p className="errorText">An unexpected error has occurred. Please refresh the page.</p>
     </div>
   );
 }
