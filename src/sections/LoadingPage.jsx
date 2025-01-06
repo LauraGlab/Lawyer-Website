bd5b356632f6f8216a48f9b34618f0fe5f6dd003
Laura Głąb <151255733+LauraGlab@users.noreply.github.com>
2025-01-06 21:55:46 +0100

    Update LoadingPage.jsx

--- src/sections/LoadingPage.jsx
+++ src/sections/LoadingPage.jsx
@@ -1,11 +1,10 @@
 import Logo from "./../assets/logo.svg";
 import "./css/LoadingPage.css";
 
-function LoadingPage() {
+export default function LoadingPage() {
   return (
     <div className="loading">
       <img className="loadingLogo" src={Logo} alt="logo"/>
     </div>
   );
 }
-export default LoadingPage;
