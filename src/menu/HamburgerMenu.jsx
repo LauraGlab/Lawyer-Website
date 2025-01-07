e87747c86bb206875a2c663fedfa07448e21014a
Laura Głąb <151255733+LauraGlab@users.noreply.github.com>
2025-01-07 16:54:36 +0100

    Update HamburgerMenu.jsx

--- src/menu/HamburgerMenu.jsx
+++ src/menu/HamburgerMenu.jsx
@@ -10,7 +10,17 @@ export default function HamburgerMenu({ onToggle, showMenu }) {
       aria-label="Toggle menu"
       aria-expanded={showMenu}
     >
-      <img src={HamburgerIcon} alt="hamburger menu icon" />
+      <svg width="50px" height="50px" viewBox="0 0 72 72" id="emoji" xmlns="http://www.w3.org/2000/svg">
+        <g id="color"/>
+        <g id="hair"/>
+        <g id="skin"/>
+        <g id="skin-shadow"/>
+        <g id="line">
+          <line x1="16" x2="56" y1="26" y2="26" fill="none" stroke="#897455" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/>
+          <line x1="16" x2="56" y1="36" y2="36" fill="none" stroke="#897455" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/>
+          <line x1="16" x2="56" y1="46" y2="46" fill="none" stroke="#897455" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/>
+        </g>
+      </svg>
     </div>
   );
 };
